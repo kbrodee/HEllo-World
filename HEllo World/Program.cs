527417e8bc67a12ea31554535974926fde4273b5
K B <kbrodee@hotmail.com>
2019-06-22 09:50:47 -0500

    Getting Close

--- HEllo World/Program.cs
+++ HEllo World/Program.cs
@@ -15,7 +15,20 @@ namespace HEllo_World
                 string Name = Console.ReadLine();
             Console.WriteLine($"Ok {Name}, Tell us what year where you born in?");
                 string YearBorn = Console.ReadLine();
-            Console.WriteLine($"To get this right you where born in {YearBorn} ?");
+            Console.WriteLine($"To get this right, {Name} you were born in {YearBorn} Right?");
+            Console.ReadKey();
+            DateTime now = DateTime.Today;
+                String CYear = now.ToString("yyyy");
+            int num1 = 0, num2 = 0;
+
+            num1 = Convert.ToInt32(CYear);
+            num2 = Convert.ToInt32(YearBorn);
+
+            int AnswerSubtract;
+
+            AnswerSubtract = num2 - num1;
+                        
+            Console.WriteLine ($"{Name} you are about "  + AnswerSubtract + " Years Old");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
